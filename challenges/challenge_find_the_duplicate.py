06c8deac7f5061acd88567907b43c8883caee0fb
duarte <gabrieldvr@outlook.com>
2023-07-24 15:41:41 -0300

    find the duplicate challenge

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,20 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    # Verifica se a entrada é uma lista não vazia
+    if not isinstance(nums, list) or len(nums) < 2:
+        return False
+
+    # Verifica se todos os elementos são inteiros positivos
+    if not all(isinstance(num, int) and num > 0 for num in nums):
+        return False
+
+    # Conjunto para rastrear os elementos já vistos
+    seen = set()
+
+    for num in nums:
+        # Se o número já foi visto, é o duplicado
+        if num in seen:
+            return num
+        seen.add(num)
+
+    # Se não houver duplicados, retorna False
+    return False
