3de8ef10951a6967abb65b4d91b7e07166d90aa3
duarte <gabrieldvr@outlook.com>
2023-07-24 15:45:19 -0300

    palindrome (iterative)

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,14 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+
+    left = 0
+    right = len(word) - 1
+
+    while left < right:
+        if word[left] != word[right]:
+            return False
+        left += 1
+        right -= 1
+
+    return True
